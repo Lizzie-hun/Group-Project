d81d2d4deabd5e5a13a8d4bedb423c49fe810bed
Thomas <nolanvargas@byui.edu>
2022-10-18 21:19:04 -0600

    fixed files

--- game/main.py
+++
@@ -1,8 +0,0 @@
-import arcade
-import globals
-from director import Director
-def main():
-    window = Director(globals.SCREEN_HEIGHT, globals.SCREEN_WIDTH, "Running Game")
-    arcade.run()
-
-main()
