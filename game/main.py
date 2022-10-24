0ce8d0872e0dd03ec83c0958422a31d65d6c52b1
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-10-24 12:21:43 -0600

    fixed filepaths

--- game/main.py
+++ game/main.py
@@ -1,6 +1,7 @@
 import arcade
-import globals
 from director import Director
+import globals
+
 def main():
     window = Director(globals.SCREEN_HEIGHT, globals.SCREEN_WIDTH, "Running Game")
     arcade.run()
