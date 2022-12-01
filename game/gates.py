2a499f1061a5588b1fbb9d643b389085a0474dae
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-12-01 14:29:33 -0700

    updated gates class

--- game/gates.py
+++ game/gates.py
@@ -1,51 +1,8 @@
 # The gates that go inside of the levels.
 import random
+import arcade
 
-class Gates():
-    def __init__(self):
-        self._wrong_ans = ""
-        self._right_ans = ''
-        self._operation = ''
-
-    def get_gate(self):
-        return (self._operation, self._right_ans, self._wrong_ans)
-
-    def create_operation(self):
-        # Putting together the operation, the right answer, and the wrong answer
-        num = random.randint(1, 4)
-        first_num = random.randint(1, 12)
-        second_num = random.randint(1, 12)
-        while first_num < second_num and num == 3 or not first_num % second_num == 0 and num == 3:
-            first_num = random.randint(1, 12)
-            second_num = random.randint(1, 12)
-            
-        if num == 1:
-            self._operation = f"{first_num} + {second_num}"
-        elif num == 2:
-            self._operation = f"{first_num} - {second_num}"
-        elif num == 3:
-            self._operation = f"{first_num} / {second_num}"
-        else:
-            self._operation = f"{first_num} * {second_num}"
-
-        self._create_answers(first_num, second_num)
-
-    def _create_answers(self, first_num, second_num):
-        operand = ''
-        for i in self._operation:
-            if i in ["+", "-", "/", "*"]:
-                operand = i
-                break
-        
-        if operand == '+':
-            self._right_ans = f"{first_num + second_num}"
-            self._wrong_ans = f"{first_num + second_num + random.randint(-10, 10)}"
-        elif operand == '-':
-            self._right_ans = f"{first_num - second_num}"
-            self._wrong_ans = f"{(first_num - second_num) + random.randint(-10, 10)}"
-        elif operand == '/':
-            self._right_ans = f"{int(first_num / second_num)}"
-            self._wrong_ans = f"{int((first_num / second_num)) + random.randint(-10, 10)}"
-        else:
-            self._right_ans = f"{first_num * second_num}"
-            self._wrong_ans = f"{(first_num * second_num) + random.randint(-10, 10)}"
+class Gate(arcade.Sprite):
+    def __init__(self, file, scaling, value):
+        super().__init__(file, scaling)
+        self.value = value
