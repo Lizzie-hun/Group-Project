fe283494f89bf98660614cab0052e233bac043df
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-12-06 11:11:12 -0700

    more work needed

--- game/gates.py
+++ game/gates.py
@@ -3,12 +3,15 @@ import random
 import arcade
 
 class Gate(arcade.Sprite):
-    def __init__(self, scaling, value):
-        number_sprite_list = []
+    def __init__(self, scaling, value):        
+        digit1_filename = f'assets/numbers/{str(value)[0]}.png'
+        digit2_filename = f'assets/numbers/{str(value)[1]}.png'
         
-        super().__init__(f'assets/numbers/0.png', scaling)
+        number_sprite_list = [digit1_filename, digit2_filename]
+
+        super().__init__(number_sprite_list, scaling)
         self.value = value
 
     def draw(self, *args, filter=None, pixelated=None, blend_function=None):
         print("hello")
-        super.draw(args, filter, pixelated, blend_function)
+        super().draw(args, filter, pixelated, blend_function)
