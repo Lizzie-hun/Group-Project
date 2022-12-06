199916264f6d546975ec6c0ee17e48a87e391fa5
sullyU <uda21002@byui.edu>
2022-12-06 12:58:03 -0700

    start on menu

--- game/main.py
+++ game/main.py
@@ -1,12 +1,13 @@
 import arcade
 from director import Director
+import menu
 import globals
 
 def main():
     """ Main function """
 
     window = arcade.Window(globals.SCREEN_WIDTH, globals.SCREEN_HEIGHT, globals.SCREEN_TITLE)
-    start_game = Director()
+    start_game = menu()
     window.show_view(start_game)
     start_game.setup()
     arcade.run()
